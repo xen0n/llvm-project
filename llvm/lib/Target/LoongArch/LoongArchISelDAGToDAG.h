344a4b0aa7efecbbd0bff0f97335dc8b269a1fdf
WANG Xuerui <git@xen0n.name>
2022-10-05 16:54:00 +0800

    [LoongArch] Don't merge FrameIndex accesses into [F]{LD,ST}X
    Otherwise eliminateFrameIndex cannot figure out how to fixup the stack
    offset with its stateless logic, because there wouldn't be an immediate
    slot for it to trivially write to, and it may not be easy to transform
    the surrounding code to make it work.
    
    This might not be the optimal fix but it fixes a fairly common crash
    when compiling moderately complex code with Clang.
    
    Differential Revision: https://reviews.llvm.org/D135251

--- llvm/lib/Target/LoongArch/LoongArchISelDAGToDAG.h
+++ llvm/lib/Target/LoongArch/LoongArchISelDAGToDAG.h
@@ -42,6 +42,7 @@ public:
                                     std::vector<SDValue> &OutOps) override;
 
   bool SelectBaseAddr(SDValue Addr, SDValue &Base);
+  bool selectNonFIBaseAddr(SDValue Addr, SDValue &Base);
 
   bool selectShiftMask(SDValue N, unsigned ShiftWidth, SDValue &ShAmt);
   bool selectShiftMaskGRLen(SDValue N, SDValue &ShAmt) {
